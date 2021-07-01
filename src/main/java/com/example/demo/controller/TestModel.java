8e5853388794b3beb12ff1288f5b9e315e5c4da5
ryohei0109-develop <ryohei0109.develop@gmail.com>
2021-07-02 08:47:58 +0900

    #add /api/hoge/, /api/hoge2/ エンドポイントを追加

---
+++ src/main/java/com/example/demo/controller/TestModel.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+public class TestModel {
+	public TestModel(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+    private String name;
+    private int age;
+}
--- src/main/java/com/example/demo/controller/WebApiController.java
+++ src/main/java/com/example/demo/controller/WebApiController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("api")
@@ -11,4 +14,26 @@ public class WebApiController {
 	private String hello() {
 		return "SpringBoot!";
 	}
+    
+	// JavaBeansを返すと自動的にJSONに変換される。
+    @RequestMapping("hoge")
+    public TestModel hoge() {
+        return new TestModel( "ほげ", 1234 );
+    }
+    
+    // 手動でJSONに変換して返す。
+    @RequestMapping(
+            value = "hoge2", 
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public String string() throws Exception {
+    	String json = "";
+    	try {
+            TestModel bean = new TestModel("もげ", 297);
+            json = new ObjectMapper().writeValueAsString(bean);    		
+    	}catch(Exception e) {
+    		System.out.println(e);
+    	}
+
+        return json;
+    }
 }
