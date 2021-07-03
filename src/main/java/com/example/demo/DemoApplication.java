e262009a87e2e7d4ec33e24338e9e34abb997df7
ryohei0109-develop <ryohei0109.develop@gmail.com>
2021-07-03 16:17:35 +0900

    #add テンプレートからの文字列生成処理を追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,18 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean("messageTemplateEngine")
+	public SpringTemplateEngine messageTemplateEngine() {
+		var resolver = new ClassLoaderTemplateResolver();
+		resolver.setTemplateMode(TemplateMode.TEXT);
+		resolver.setPrefix("templates/messages/"); // src/main/resources/templates/messages
+		resolver.setSuffix(".message");
+		resolver.setCharacterEncoding("UTF-8");
+		resolver.setCacheable(true);
+
+		var engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(resolver);
+
+		return engine;
+	}
 }
---
+++ src/main/java/com/example/demo/controller/TemplateController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+/**
+ * Tyhmeleafエンジンを使用して文字列を生成する処理を実装したクラスです。
+ * 
+ * @author ryohei
+ *
+ */
+@RestController
+@RequestMapping("template")
+public class TemplateController {
+	@Qualifier("messageTemplateEngine") 
+	@Autowired()
+	SpringTemplateEngine engine;
+
+	@GetMapping("test")
+	public String test() {
+		LocalDateTime now = LocalDateTime.now();
+
+		String hour = now.getHour() + "時";
+		String timestamp = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(now);
+		return process(hour, timestamp);
+	}
+
+	private String process(String hour, String time) {
+	    var context = new Context();
+	    context.setVariable( "hour", hour );
+	    context.setVariable( "timestamp", time );
+
+	    final String message = this.engine.process( "hoge", context );
+
+	    return message;
+	}
+}
